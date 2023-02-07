a89c7aee13446c3fb3f1152ee8924196fbe94378
Vito Tenza <vito.tenza.progettopa.it>
2023-02-07 14:41:18 +0100

    FIx layout

--- src/app/pages/home-page/pages/panoramica-issues/panoramica-issues.component.ts
+++ src/app/pages/home-page/pages/panoramica-issues/panoramica-issues.component.ts
@@ -11,38 +11,59 @@ export class PanoramicaIssuesComponent implements OnInit {
 
   data: any;
   allIssues: Array<Issues> = [];
+  dataTableIssues: Array<Issues> = [];
+
 
   constructor(
     private issuesService: IssuesService
   ) { }
 
   async ngOnInit() {
+    this.dataTableIssues = [];
     this.allIssues = await this.issuesService.getAllIssues().toPromise();
     this.data = {
-      labels: ['Todo','In Progress','Done', 'Deliverable'],
+      labels: ['Todo', 'In Progress', 'Done', 'Deliverable'],
       datasets: [
-          {
-              data: [
-                this.allIssues.filter(i => i.state === 'TODO').length, 
-                this.allIssues.filter(i => i.state === 'IN_PROGRESS').length, 
-                this.allIssues.filter(i => i.state === 'DONE').length,
-                this.allIssues.filter(i => i.state === 'DELIVERABLE').length
-              ],
-              backgroundColor: [
-                  "#42A5F5",
-                  "#ff6699",
-                  "#FFA726",
-                  "#66BB6A",
-              ],
-              hoverBackgroundColor: [
-                  "#64B5F6",
-                  "#ff99bb",
-                  "#FFB74D",
-                  "#81C784",
-              ]
-          }
+        {
+          data: [
+            this.allIssues.filter(i => i.state === 'TODO').length,
+            this.allIssues.filter(i => i.state === 'IN_PROGRESS').length,
+            this.allIssues.filter(i => i.state === 'DONE').length,
+            this.allIssues.filter(i => i.state === 'DELIVERABLE').length
+          ],
+          backgroundColor: [
+            "#42A5F5",
+            "#ff6699",
+            "#FFA726",
+            "#66BB6A",
+          ],
+          hoverBackgroundColor: [
+            "#64B5F6",
+            "#ff99bb",
+            "#FFB74D",
+            "#81C784",
+          ]
+        }
       ]
-  };
+    };
+  }
+
+  chartClick(event) {
+    const filterValue = event.element._model.label;
+    switch (filterValue) {
+      case 'Todo':
+        this.dataTableIssues = this.allIssues.filter(i => i.state === 'TODO');
+        break;
+      case 'In Progress':
+        this.dataTableIssues = this.allIssues.filter(i => i.state === 'IN_PROGRESS');
+        break;
+      case 'Done':
+        this.dataTableIssues = this.allIssues.filter(i => i.state === 'DONE');
+        break;
+      case 'Deliverable':
+        this.dataTableIssues = this.allIssues.filter(i => i.state === 'DELIVERABLE');
+        break;
+    }
   }
 
 }
